56fa6e1913917965c373c48c2baba3a60b4a888b
Lucas Gomes Dantas <lucas_gomesdantas@hotmail.com>
2018-09-11 16:23:38 -0300

    Getting code to work more properly.

--- filer/items.py
+++ filer/items.py
@@ -22,3 +22,32 @@ class FilerDetail(scrapy.Item):
     status_nota = scrapy.Field()
     data_status_nota = scrapy.Field()
     identificador_interno = scrapy.Field()
+
+
+class FilerDetailCnaes(scrapy.Item):
+    cnae_cnpj_cpf_emitente = scrapy.Field()
+    cnae_codigo_atividade = scrapy.Field()
+
+
+class FilerPartner(scrapy.Item):
+    socio_cnpj_cpf_emitente = scrapy.Field()
+    socio_cpf_socio_emitente = scrapy.Field()
+    socio_nome_socio = scrapy.Field()
+    socio_eh_adm = scrapy.Field()
+
+
+class FilerEmissor(scrapy.Item):
+    emissor_cnpj_cpf_emitente = scrapy.Field()
+    emissor_razao_social = scrapy.Field()
+    emissor_nome_fantasia = scrapy.Field()
+    emissor_endereco = scrapy.Field()
+    emissor_bairro = scrapy.Field()
+    emissor_cep = scrapy.Field()
+    emissor_cidade = scrapy.Field()
+    emissor_uf = scrapy.Field()
+    emissor_codigo_atividade = scrapy.Field()
+    emissor_data_inicio = scrapy.Field()
+    emissor_data_encerramento = scrapy.Field()
+    emissor_tipo_contribuinte = scrapy.Field()
+
+
--- filer/spiders/filer.py
+++ filer/spiders/filer.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from filer.items import FilerDetail
+from filer.items import FilerDetailCnaes
+from filer.items import FilerPartner
+from filer.items import FilerEmissor
 
 
 class FilerSpider(scrapy.Spider):
@@ -9,7 +12,7 @@ class FilerSpider(scrapy.Spider):
     start_urls = ['http://example.com/']
 
     def parse(self, response):
-        with open("./misc/receipt.txt") as file:
+        with open("./docs/receipt.txt") as file:
             data = file.read()
             size = len(data)
             charAt = 0
@@ -43,4 +46,57 @@ class FilerSpider(scrapy.Spider):
                     charAt += 14
                     receipt["identificador_interno"] = data[charAt:charAt + 10]
                     charAt += 11  # Original 10 + 1 (that leads to the other position)
+                    yield receipt
+                elif data[charAt] == "6":
+                    charAt += 1
+                    receipt = FilerDetailCnaes()
+                    receipt["cnae_cnpj_cpf_emitente"] = data[charAt:charAt + 14]
+                    charAt += 14
+                    receipt["cnae_codigo_atividade"] = data[charAt:charAt + 7]
+                    charAt += 8  # Original 7 + 1
+                    yield receipt
+                elif data[charAt] == "5":
+                    charAt += 1
+                    receipt = FilerPartner()
+                    receipt["socio_cnpj_cpf_emitente"] = data[charAt:charAt + 14]
+                    charAt += 14
+                    receipt["socio_cpf_socio_emitente"] = data[charAt:charAt + 11]
+                    charAt += 11
+                    receipt["socio_nome_socio"] = data[charAt:charAt + 60]
+                    charAt += 60
+                    receipt["socio_eh_adm"] = data[charAt:charAt + 1]
+                    charAt += 2  # Original 1 + 1
+                    yield receipt
+                elif data[charAt] == "4":
+                    charAt += 1
+                    receipt = FilerEmissor()
+                    receipt["emissor_cnpj_cpf_emitente"] = data[charAt:charAt + 14]
+                    charAt += 14
+                    receipt["emissor_razao_social"] = data[charAt:charAt + 100]
+                    charAt += 100
+                    receipt["emissor_nome_fantasia"] = data[charAt:charAt + 50]
+                    charAt += 50
+                    receipt["emissor_endereco"] = data[charAt:charAt + 80]
+                    charAt += 80
+                    receipt["emissor_bairro"] = data[charAt:charAt + 50]
+                    charAt += 50
+                    receipt["emissor_cep"] = data[charAt:charAt + 8]
+                    charAt += 8
+                    receipt["emissor_cidade"] = data[charAt:charAt + 50]
+                    charAt += 50
+                    receipt["emissor_uf"] = data[charAt:charAt + 2]
+                    charAt += 2
+                    receipt["emissor_codigo_atividade"] = data[charAt:charAt + 8]
+                    charAt += 8
+                    receipt["emissor_data_inicio"] = data[charAt:charAt + 8]
+                    charAt += 8
+                    receipt["emissor_data_encerramento"] = data[charAt:charAt + 8]
+                    charAt += 8
+                    endIter = 0
+                    beginIter = charAt
+                    while data[beginIter:charAt + endIter + 1] != "\n":
+                        endIter += 1
+                        beginIter += 1
+                    receipt["emissor_tipo_contribuinte"] = data[charAt:charAt + endIter]
+                    charAt += endIter + 1
                     yield receipt
